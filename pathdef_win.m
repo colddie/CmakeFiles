8af6c06759bc10b5dc57cc3488c3807a3a16bcec
colddie <colddiesun@gmail.com>
2019-12-14 17:35:09 -0500

    update

---
+++ pathdef_win.m
@@ -0,0 +1,12 @@
+addpath(genpath('X:\storage0\home\tsun\code\matlab\general'))
+addpath(genpath('X:\storage0\home\tsun\code\matlab\viewer3dgui'))
+addpath(genpath('X:\storage0\home\tsun\code\matlab\radial2D'))
+addpath(genpath('X:\storage0\home\tsun\code\matlab\e7tool_matlab_from_JO'))
+addpath(genpath('X:\storage0\home\tsun\code\matlab\REST_V1.8_130615'))
+%addpath(genpath('X:\storage0\home\tsun\code\matlab\PETRECON_MP24_R03_REL_1_28_MRAC_pw-BestToolboxEver01242017'))
+% addpath(genpath('\storage3\MLAA\GEtoolbox\PETRECON_MP24_R03_REL_1_28_MRAC_pw-BestToolboxEver01242017\PETRECON_MP24_R03_REL_1_28_MRAC'))
+addpath(genpath('/storage0/home/tsun/code/matlab/Duetto/DUETTO_REL1.1.3/duetto'))
+%addpath(genpath('\storage0\home\tsun\code\matlab\nm_signa'))
+%run('\storage0\home\tsun\code\matlab\fessler\irt\setup.m')
+
+diary('log.txt')
